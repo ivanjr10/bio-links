691a3c38a528ccddcf9a53ffc4a4fda0404507f3
Ivan Júnior <ivan.figueiredo.jr@gmail.com>
2025-08-14 17:04:11 -0300

    criando rota de login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function login()
+    {
+        dd(request()->all());
+    }
 }
